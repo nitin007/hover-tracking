a279aec73e2506529c1957c85f572d2cd98ce96e
Nitin Gupta <nitin.gupta@vinsol.com>
2014-01-21 13:36:55 +0530

    Revert "xhr constaint removed - revert"
    This reverts commit ae9e93a02a5b4dd56c8c3072c9d37ca4a76ddd81.

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@
  */
 
 exports.index = function(req, res) {
-  // if (req.xhr) {
-    console.log(req.body);
+  if (req.xhr) {
     db.positions.save({name: req.body.name, coords: req.body.coords}, function(err, saved) {
       if (err || !saved) {
         res.end("Coords not saved");
@@ -19,10 +18,9 @@ exports.index = function(req, res) {
         });
       }
     });
-  // } else {
-  //     console.log(req.body);
-  //     res.render('index', {title: 'Hover Tracker'});
-  //   }
+  } else {
+      res.render('index', {title: 'Hover Tracker'});
+    }
 }
 
 exports.hoverCounts = function(req, res){
